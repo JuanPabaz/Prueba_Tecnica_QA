8acc466b1f139d66526a329108467874b1783af1
JuanPabaz <jpgthf@gmail.com>
2023-09-13 13:22:12 -0500

    Se agrega la prueba tecnica

---
+++ main.py
@@ -0,0 +1,11 @@
+def sort_list(input_list):
+    print(input_list)
+    for i in range(len(input_list)):
+        for j in range(len(input_list) - 1):
+            if input_list[j] > input_list[j + 1]:
+                input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
+    print(input_list)
+    return input_list
+
+input_list = [2,1,5,6,3,10]
+sort_list(input_list)
